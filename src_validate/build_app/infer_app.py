2b51099a33ea4d88e5c1c2b3a249d8d469179967
ParhomEsmaeili <parhom.esmaeili@gmail.com>
2026-01-19 13:54:31 +0000

    updating the app initialisation to match change to the validation framework

--- src_validate/build_app/infer_app.py
+++ src_validate/build_app/infer_app.py
@@ -172,7 +172,7 @@ def load_sammed2d(checkpoint_path, encoder_model_type, image_size, device="cuda"
 
 class InferApp:
  
-    def __init__(self, infer_device):
+    def __init__(self, infer_device, algorithm_state, enable_adaptation, algo_cache_name):
         
         self.sanity_check = False 
         self.sanity_slice_check = 510
